af6c7c3cea5ea0e73353bdf887bff3478ac3f6a4
Coco0719 <ejsvk3284@gmail.com>
2020-05-18 02:12:09 +0900

    UI 작업 및 리액트 라우터 적용
    - 리액트 라우터 적용
    - 로그인 페이지
    - 회원가입 페이지
    - 기타 페이지

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import NotFound from './pages/errors/NotFound'
+
+//import './App.css';
+
 
 function App() {
   return (
-
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
